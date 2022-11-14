4f295842419de55b94c36fa6daab9f8fb0e5299f
Sibling Riot <tcstamp2001@gmail.com>
2022-11-13 19:08:05 -0500

    added template json file

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
+import sys
+
+# open local JSON file
+sys.stdout=open("complimentsA.json","w")
+
 # input
 string = str(input())
   
 # output
 print(string)
+
+# save output to file
+sys.stdout.close()
+
