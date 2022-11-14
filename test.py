fed88803668da5e7de841c3c2974406f0ee6d86b
Sibling Riot <tcstamp2001@gmail.com>
2022-11-14 15:39:23 -0500

    updated script

--- test.py
+++ test.py
@@ -1,26 +1,27 @@
 # Import JSON module to allow user to "read and write" the JSON file
 import json
 
+compliments=[]
+
 # Input message in terminal
 string = str(input())
 
 # Output message as a string
 print(string)
 
+compliments.append(string)
+
 # Open local JSON file (r+ means read and write)
 with open('complimentsA.json', 'r+') as f:
     data = json.load(f)
 
-# Insert output message into each respective category   
-    data['anytime'] = string
-    data['morning'] = string
-    data['afternoon'] = string
-    data['evening'] = string
+# Assign string value into "anytime"
+    data['anytime'] = compliments
    
 # Reset file position to beginning   
     f.seek(0)
 
-# Insert data in file by converting python object to json object  
+# Insert doutput message in file by converting python object to json object  
     json.dump(data, f, indent=2)
    
 # Remove remaining part   
