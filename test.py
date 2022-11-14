02cd36f26f58d5f26ad86cbd559de7a9b0221307
Sibling Riot <tcstamp2001@gmail.com>
2022-11-14 12:44:02 -0500

    updated test.py

--- test.py
+++ test.py
@@ -7,10 +7,13 @@ string = str(input())
 # Output message as a string
 print(string)
 
-# open local JSON file & insert string as an "anytime" compliment at the beginning of the JSON file (r+ means read and write)
+# open local JSON file & insert string as an "anytime", "morning", "afternoon", and "evening" compliment at the beginning of the JSON file (r+ means read and write)
 with open('complimentsA.json', 'r+') as f:
     data = json.load(f)
     data['anytime'] = string
+    data['morning'] = string
+    data['afternoon'] = string
+    data['evening'] = string
     f.seek(0)
     json.dump(data, f, indent=2)
     f.truncate()
