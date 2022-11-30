c80850490b8b5cdf79c2c2d962df05100c4498a0
SiblingRiot <113385409+SiblingRiot@users.noreply.github.com>
2022-11-30 14:14:12 -0500

    Create testA.py

---
+++ testA.py
@@ -0,0 +1,31 @@
+# Import JSON module to allow user to "read and write" the JSON file
+import json
+
+compliments=[]
+
+# Input message in terminal
+string = str(input())
+
+# Clean up the string
+newstring = string.rstrip()
+
+# Output message as a string
+print(newstring)
+
+compliments.append(newstring)
+
+# Open local JSON file (r+ means read and write)
+with open('complimentsA.json', 'r+') as f:
+    data = json.load(f)
+
+# Assign string value into "anytime"
+    data['anytime'] = compliments
+   
+# Reset file position to beginning   
+    f.seek(0)
+
+# Insert doutput message in file by converting python object to json object  
+    json.dump(data, f, indent=2)
+   
+# Remove remaining part   
+    f.truncate()
